5f520af3f46a511c1b305324c182ab72e56deffd
Andrey Fomin <a48671@gmail.com>
2019-04-29 10:53:59 +0300

    build

---
+++ precache-manifest.ceaaa195cd23174f3bf90415f5c29337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e916fdaad17987e451d50de1481c8de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84c40fa6ad9e7eb6f3cf",
+    "url": "/static/css/main.f396bae2.chunk.css"
+  },
+  {
+    "revision": "10e20f57de38e9206c94",
+    "url": "/static/js/2.7223aacb.chunk.js"
+  },
+  {
+    "revision": "84c40fa6ad9e7eb6f3cf",
+    "url": "/static/js/main.2a7d52a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
